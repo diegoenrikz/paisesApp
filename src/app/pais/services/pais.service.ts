1fcc26bb0e66dabd8ffb7a7635a33be429213d60
Diego Enriquez <denriquez@cobiscorp.com>
2022-02-06 23:03:10 -0500

    Se agreaga codigo fuente de paisesApp

---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Country } from '../interfaces/pais.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  get httpParams(){
+    return new HttpParams().set('fields', 'name,capital,population,flags,cca2,ccn3,cca3' );
+  }
+
+  constructor(private http: HttpClient) { }
+
+  buscarPais(termino: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/name/${termino}`;
+
+    return this.http.get<Country[]>(url, {params: this.httpParams});
+
+  }
+
+  buscarCapital(termino: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/capital/${termino}`;
+
+    return this.http.get<Country[]>(url, {params: this.httpParams});
+
+  }
+
+  getPaisPorAlpha(id: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/alpha/${id}`;
+
+    return this.http.get<Country[]>(url);
+
+  }
+
+  buscarRegion (region: string): Observable<Country[]> {
+    
+    const url = `${this.apiUrl}/region/${region}`;
+
+    return this.http.get<Country[]>(url, {params: this.httpParams})
+    .pipe(
+      tap(console.log)
+    );
+
+  }
+
+}
